e472711ab47955555c0c20004ee7c875384909c9
Cengiz TORU <cengiztoru@gmail.com>
2021-10-18 16:00:49 +0300

    Account Kit Activity created

---
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/AccountKitActivity.kt
@@ -0,0 +1,12 @@
+package com.cengiztoru.hmscorekits.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.hmscorekits.R
+
+class AccountKitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_kit)
+    }
+}
