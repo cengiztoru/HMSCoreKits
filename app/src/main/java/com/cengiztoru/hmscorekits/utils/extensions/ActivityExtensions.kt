fc6676c1b404f326c8712ebd7deeedbdcf77819e
Cengiz TORU <cengiztoru@gmail.com>
2021-10-18 16:02:58 +0300

    A new ext function added for starting activities

--- app/src/main/java/com/cengiztoru/hmscorekits/utils/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/utils/extensions/ActivityExtensions.kt
@@ -1,6 +1,7 @@
 package com.cengiztoru.hmscorekits.utils.extensions
 
 import android.app.Activity
+import android.content.Intent
 import android.widget.Toast
 import androidx.annotation.StringRes
 
@@ -15,4 +16,8 @@ fun Activity.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
 
 fun Activity.showToast(@StringRes messageRes: Int, duration: Int = Toast.LENGTH_SHORT) {
     applicationContext.showToast(messageRes, duration)
+}
+
+inline fun <reified T : Activity> Activity.startActivity(block: Intent.() -> Unit = {}) {
+    startActivity(Intent(this, T::class.java).apply(block))
 }
