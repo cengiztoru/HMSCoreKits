8a7bebf2ae9a96667a9c5e4365709c688f10353b
Cengiz TORU <cengiztoru@gmail.com>
2021-10-18 17:23:57 +0300

    SignIn with Huawei ID done

--- app/src/main/java/com/cengiztoru/hmscorekits/ui/account_kit/AccountKitActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/account_kit/AccountKitActivity.kt
@@ -1,12 +1,83 @@
 package com.cengiztoru.hmscorekits.ui.account_kit
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.cengiztoru.hmscorekits.R
+import com.cengiztoru.hmscorekits.databinding.ActivityAccountKitBinding
+import com.cengiztoru.hmscorekits.utils.extensions.showToast
+import com.huawei.hms.common.ApiException
+import com.huawei.hms.support.account.AccountAuthManager
+import com.huawei.hms.support.account.request.AccountAuthParams
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper
+import com.huawei.hms.support.account.service.AccountAuthService
+
+const val TAG = "AccountKitActivity"
 
 class AccountKitActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAccountKitBinding
+
+    private var mAuthManager: AccountAuthService? = null
+    private var mAuthParam: AccountAuthParams? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_kit)
+
+        inflateViews()
+        setListeners()
+
     }
+
+//region signIn with HuaweiID
+
+    private var singInResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val authAccountTask = AccountAuthManager.parseAuthResultFromIntent(result.data)
+                if (authAccountTask.isSuccessful) {
+                    val authAccount = authAccountTask.result
+                    showToast("Welcome ${authAccount.displayName}")
+                    printMessage("Name: ${authAccount.displayName} \nAccessToken:${authAccount.accessToken}")
+                } else {
+                    Log.e(
+                        TAG,
+                        "signIn failed: " + (authAccountTask.exception as ApiException).statusCode
+                    )
+                }
+            }
+        }
+
+    private fun signIn() {
+        mAuthParam = AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+            .setIdToken()
+            .setAccessToken()
+            .createParams()
+        mAuthManager = AccountAuthManager.getService(this@AccountKitActivity, mAuthParam)
+        singInResultLauncher.launch(mAuthManager?.signInIntent)
+    }
+
+//endregion
+
+//region common functions
+
+    private fun inflateViews() {
+        mBinding = ActivityAccountKitBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    private fun setListeners() {
+        mBinding.btnAccountSignin.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun printMessage(text: String) {
+        val beforeText = mBinding.tvLogger.text
+        mBinding.tvLogger.text = (if (beforeText.isNotBlank()) "$beforeText \n\n" else "") + text
+    }
+
+//endregion
+
 }
