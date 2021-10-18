1f4c80c4555abe60600c57a8975d35e014781b96
Cengiz TORU <cengiztoru@gmail.com>
2021-10-18 13:03:41 +0300

    A scrollable structure has been prepared that will contain the buttons to go to hms kits screens

--- app/src/main/java/com/cengiztoru/hmscorekits/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.cengiztoru.hmscorekits
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.hmscorekits.databinding.ActivityMainBinding
+import com.cengiztoru.hmscorekits.utils.extensions.showToast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initViews()
+        setListeners()
+
+    }
+
+    private fun initViews() {
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    private fun setListeners() {
+        mBinding.btnMainTest.setOnClickListener {
+            showToast("Main Activity Test Button Clicked")
+        }
     }
 }
