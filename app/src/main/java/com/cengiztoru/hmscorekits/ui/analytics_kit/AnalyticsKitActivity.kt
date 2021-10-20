5bc3c5a52cae456dba60433e1ae7715e144f8b32
Cengiz TORU <cengiztoru@gmail.com>
2021-10-20 18:47:24 +0300

    [HMS Analytics Kit] Obtaining Network Access Authorization from User Done

--- app/src/main/java/com/cengiztoru/hmscorekits/ui/analytics_kit/AnalyticsKitActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/analytics_kit/AnalyticsKitActivity.kt
@@ -1,11 +1,15 @@
 package com.cengiztoru.hmscorekits.ui.analytics_kit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.cengiztoru.hmscorekits.databinding.ActivityAnalyticsBinding
+import com.huawei.agconnect.common.network.AccessNetworkManager
 import com.huawei.hms.analytics.HiAnalytics
 import com.huawei.hms.analytics.HiAnalyticsInstance
 import com.huawei.hms.analytics.HiAnalyticsTools
+
+private const val TAG = "AnalyticsKitActivity"
 
 class AnalyticsKitActivity : AppCompatActivity() {
 
@@ -23,7 +27,14 @@ class AnalyticsKitActivity : AppCompatActivity() {
 //region HMS Analytics Kit Functions
 
     private fun setUserProfile(key: String, value: String) {
+        Log.i(TAG, "set user profile $key : #$value")
         analyticsInstance.setUserProfile(key, value)
+    }
+
+
+    private fun allowNetworkAccess() {
+        Log.i(TAG, "Allowing Internet Access Started")
+        AccessNetworkManager.getInstance().setAccessNetwork(true)
     }
 
 //endregion
@@ -33,6 +44,8 @@ class AnalyticsKitActivity : AppCompatActivity() {
         mBinding = ActivityAnalyticsBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        // During SDK initialization, a network request will be sent. You are advised to initialize the SDK after users agree to the privacy agreement.
+        allowNetworkAccess()
 
         // Enable Analytics Kit logging.
         // Initialize Analytics Kit in the main thread by using the onCreate() method of the first app activity.
@@ -45,7 +58,12 @@ class AnalyticsKitActivity : AppCompatActivity() {
         mBinding.btnPushEvent.setOnClickListener {
             setUserProfile("favorite_sport", mBinding.etFavoriteSport.text.toString())
         }
+
+        mBinding.btnAllowNetworkAccess.setOnClickListener {
+            allowNetworkAccess()
+        }
     }
+
 
 //endregion
 }
