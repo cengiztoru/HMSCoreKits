d76632a8f17d937795241e9c658a4b6064f06706
Cengiz TORU <cengiztoru@gmail.com>
2021-10-20 17:15:19 +0300

    [HMS Analytics Kit] Demo Screen Files Created

---
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/analytics_kit/AnalyticsKitActivity.kt
@@ -0,0 +1,13 @@
+package com.cengiztoru.hmscorekits.ui.analytics_kit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cengiztoru.hmscorekits.R
+
+class AnalyticsKitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analytics)
+
+    }
+}
