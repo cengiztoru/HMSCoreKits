b7215eb42514332bcc36295c628f0c5e36d6919b
Cengiz TORU <cengiztoru@gmail.com>
2021-10-20 21:44:51 +0300

    [HMS Analytics Kit] Prepared Some Predefined Event Type Examples

--- app/src/main/java/com/cengiztoru/hmscorekits/ui/analytics_kit/AnalyticsKitActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/analytics_kit/AnalyticsKitActivity.kt
@@ -8,6 +8,10 @@ import com.huawei.agconnect.common.network.AccessNetworkManager
 import com.huawei.hms.analytics.HiAnalytics
 import com.huawei.hms.analytics.HiAnalyticsInstance
 import com.huawei.hms.analytics.HiAnalyticsTools
+import com.huawei.hms.analytics.type.HAEventType.ADDPRODUCT2CART
+import com.huawei.hms.analytics.type.HAEventType.REGISTERACCOUNT
+import com.huawei.hms.analytics.type.HAParamType
+import kotlin.random.Random
 
 private const val TAG = "AnalyticsKitActivity"
 
@@ -42,6 +46,10 @@ class AnalyticsKitActivity : AppCompatActivity() {
         analyticsInstance.clearCachedData()
     }
 
+    private fun sendAnalyticEvent(key: String, bundle: Bundle) {
+        analyticsInstance.onEvent(key, bundle)
+    }
+
 //endregion
 
     //region common functions
@@ -60,7 +68,7 @@ class AnalyticsKitActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        mBinding.btnPushEvent.setOnClickListener {
+        mBinding.btnUserProfileEvent.setOnClickListener {
             setUserProfile("favorite_sport", mBinding.etFavoriteSport.text.toString())
         }
 
@@ -71,6 +79,62 @@ class AnalyticsKitActivity : AppCompatActivity() {
         mBinding.btnClearData.setOnClickListener {
             clearCacheData()
         }
+
+//region PREDEFINED EVENT EXAMPLE
+
+//      PREDEFINED EVENT TYPE LIST
+//      https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/android-api-haeventtype-0000001051318956
+
+//      PREDEFINED PARAMETER TYPE LIST
+//      https://developer.huawei.com/consumer/en/doc/development/HMSCore-References/android-api-haparamtype-0000001051319011
+
+        mBinding.btnAddToCart.setOnClickListener {
+
+            val addToCartBundle = Bundle().apply {
+                putString(HAParamType.PRODUCTID, "11102021")
+                putString(HAParamType.PRODUCTNAME, "HUAWEI MateBook X Pro 2021")
+                putString(HAParamType.CATEGORY, "laptops")
+                putLong(HAParamType.QUANTITY, 1)
+                putDouble(HAParamType.PRICE, 1299.99)
+                putString(HAParamType.CURRNAME, "EUR")
+                putString(HAParamType.PLACEID, "UK")
+
+            }
+
+            sendAnalyticEvent(ADDPRODUCT2CART, addToCartBundle)
+
+        }
+
+        mBinding.btnRegister.setOnClickListener {
+
+            val registerBundle = Bundle().apply {
+                putString(HAParamType.USERID, "11102021")
+                putString(HAParamType.NAME, "Cengiz TORU")
+                putString(HAParamType.USERTYPE, "blablabla")
+                putString(HAParamType.USERGROUPNAME, "blablabla")
+            }
+
+            sendAnalyticEvent(REGISTERACCOUNT, registerBundle)
+
+        }
+
+        var score = 0
+
+        mBinding.btnSubmitScore.setOnClickListener {
+
+            score += Random.nextInt(0, 100)
+
+            sendAnalyticEvent(REGISTERACCOUNT,
+                Bundle().apply {
+                    putInt(HAParamType.SCORE, score)
+                }
+            )
+
+            mBinding.btnSubmitScore.text = "Submit Score ($score)"
+        }
+
+//endregion
+
     }
 
 
