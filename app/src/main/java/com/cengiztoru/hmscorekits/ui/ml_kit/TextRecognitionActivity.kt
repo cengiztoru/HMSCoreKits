6e375329647cc087906cbf89b623fd7c6cf7df0f
Cengiz TORU <cengiztoru@gmail.com>
2021-11-01 23:47:26 +0300

    [HMS ML KIT - DOCUMENT RECOGNITION] Document recognition implemented with multiple document images

--- app/src/main/java/com/cengiztoru/hmscorekits/ui/ml_kit/TextRecognitionActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/ml_kit/TextRecognitionActivity.kt
@@ -6,15 +6,16 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.cengiztoru.hmscorekits.R
 import com.cengiztoru.hmscorekits.databinding.ActivityMlkitTextRecognitionBinding
-import com.cengiztoru.hmscorekits.utils.extensions.Constants
 import com.cengiztoru.hmscorekits.utils.extensions.startActivity
 import com.huawei.hmf.tasks.Task
 import com.huawei.hms.mlsdk.MLAnalyzerFactory
-import com.huawei.hms.mlsdk.common.MLApplication
 import com.huawei.hms.mlsdk.common.MLException
 import com.huawei.hms.mlsdk.common.MLFrame
+import com.huawei.hms.mlsdk.document.MLDocumentSetting
 import com.huawei.hms.mlsdk.text.MLRemoteTextSetting
 import com.huawei.hms.mlsdk.text.MLText
+import java.io.IOException
+import kotlin.random.Random
 
 
 class TextRecognitionActivity : AppCompatActivity() {
@@ -29,10 +30,6 @@ class TextRecognitionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMlkitTextRecognitionBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-
-        //Set Access token for using cloud based services
-        MLApplication.getInstance().setAccessToken(Constants.API_KEY)
-
 
         setListeners()
     }
@@ -49,7 +46,7 @@ class TextRecognitionActivity : AppCompatActivity() {
         val task: Task<MLText> = analyzer.asyncAnalyseFrame(frame)
         task.addOnSuccessListener {
             // Processing for successful recognition.
-            printLog("TEXT RECOGNITION SUCCESS \n ${it.stringValue}")
+            printLog(it.stringValue)
 
 //      After the recognition is complete, stop the analyzer to release recognition resources
             analyzer.stop()
@@ -91,7 +88,7 @@ class TextRecognitionActivity : AppCompatActivity() {
         val task = analyzer.asyncAnalyseFrame(frame)
         task.addOnSuccessListener {
             // Recognition success.
-            printLog("TEXT RECOGNITION ON CLOUD SUCCESS \n ${it.stringValue}")
+            printLog(it.stringValue)
 
             //After the recognition is complete, stop the analyzer to release recognition resources
             analyzer.stop()
@@ -113,15 +110,77 @@ class TextRecognitionActivity : AppCompatActivity() {
 
 //endregion
 
+//region DOCUMENT RECOGNITION
+
+    private fun documentRecognition() {
+        // Create a language set.
+        val languageList: MutableList<String> = ArrayList()
+        languageList.add("tr")
+        languageList.add("en")
+
+        // Set parameters.
+        val setting = MLDocumentSetting.Factory()
+            // Specify the languages that can be recognized, which should comply with ISO 639-1.
+            .setLanguageList(languageList)
+
+            // Set the format of the returned text border box.
+            // MLRemoteTextSetting.NGON: Return the coordinates of the four corner points of the quadrilateral.
+            // MLRemoteTextSetting.ARC: Return the corner points of a polygon border in an arc. The coordinates of up to 72 corner points can be returned.
+            .setBorderType(MLRemoteTextSetting.ARC)
+            .create()
+
+        val analyzer = MLAnalyzerFactory.getInstance().getRemoteDocumentAnalyzer(setting)
+
+        // Create an MLFrame object using the bitmap, which is the image data in bitmap format.
+        val documentId =
+            if (Random.nextBoolean()) R.drawable.document_recognition else R.drawable.einstein
+        mBinding.imgDocumentRecognition.setImageResource(documentId)
+        val bitmap = BitmapFactory.decodeResource(resources, documentId)
+        val frame = MLFrame.fromBitmap(bitmap)
+        val task = analyzer.asyncAnalyseFrame(frame)
+        task.addOnSuccessListener {
+            // Recognition success.
+            printLog(it.stringValue)
+
+            //After the recognition is complete, stop the analyzer to release recognition resources
+            try {
+                analyzer.stop()
+            } catch (e: IOException) {
+                // Exception handling.
+                printLog("addOnSuccessListener,analyzer.stop() CATCH  \n ${e.localizedMessage}")
+            }
+        }.addOnFailureListener { e ->
+            // If the recognition fails, obtain related exception information.
+            try {
+                // Recognition failure.
+                val mlException = e as MLException
+                printLog("TEXT RECOGNITION ON CLOUD FAILURE. Message: ${mlException.message} Error Code : ${mlException.errCode}")
+
+                //After the recognition is complete, stop the analyzer to release recognition resources
+                analyzer.stop()
+            } catch (error: Exception) {
+                // Handle the conversion error.
+                printLog("addOnFailureListener, analyzer.stop() CATCH  \n ${e.localizedMessage}")
+            }
+        }
+    }
+
+//endregion
 
     private fun setListeners() {
         mBinding.btnTextRecognition.setOnClickListener {
 //            localTextRecognition()
             cloudTextRecognition()
         }
+
         mBinding.btnLiveRecognition.setOnClickListener {
             startActivity<LiveTextRecognition>()
         }
+
+        mBinding.btnDocumentRecognition.setOnClickListener {
+            documentRecognition()
+        }
+
     }
 
     private fun printLog(log: String) {
