e14dd1fd83e7c2bcf1f5b0e81e33eb6f2c7d9363
Cengiz TORU <cengiztoru@gmail.com>
2021-11-05 09:21:35 +0300

    [HMS ML KIT] RunTime permissions requested from user

--- app/src/main/java/com/cengiztoru/hmscorekits/ui/ml_kit/FaceBodyRelatedActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/ml_kit/FaceBodyRelatedActivity.kt
@@ -1,20 +1,27 @@
 package com.cengiztoru.hmscorekits.ui.ml_kit
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.cengiztoru.hmscorekits.R
 import com.cengiztoru.hmscorekits.databinding.ActivityMllkitFaceBodyServicesBinding
+import com.cengiztoru.hmscorekits.utils.extensions.isAllPermissionsGranted
+import com.cengiztoru.hmscorekits.utils.extensions.showToast
 import com.huawei.hms.mlsdk.livenessdetection.MLLivenessCapture
 import com.huawei.hms.mlsdk.livenessdetection.MLLivenessCaptureResult
 
 class FaceBodyRelatedActivity : AppCompatActivity() {
 
+    private val PERMISSION_REQUEST_CODE = 51120212
     private lateinit var mBinding: ActivityMllkitFaceBodyServicesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMllkitFaceBodyServicesBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+        permissionOperations()
         setListeners()
     }
 
@@ -36,6 +43,38 @@ class FaceBodyRelatedActivity : AppCompatActivity() {
         val capture = MLLivenessCapture.getInstance()
         capture.startDetect(this, callback)
     }
+
+//region HANDLING RUNTIME PERMISSIONS
+
+    private fun permissionOperations() {
+        val neededPermissions = arrayOf(Manifest.permission.CAMERA)
+
+        if (isAllPermissionsGranted(neededPermissions).not()) {
+            ActivityCompat.requestPermissions(
+                this, neededPermissions,
+                PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            printLog("onRequestPermissionsResult: CAMERA PERMISSIONS GRANTED")
+        } else {
+            printLog("onRequestPermissionsResult:  USER NOT APPROVED PERMISSIONS")
+            showToast("Please grant to permissions for using service")
+        }
+
+    }
+
+//endregion
 
 
     private fun setListeners() {
