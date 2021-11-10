1b06ac60fa64eaa67dcfdc0b158c50acc9bfd480
Cengiz TORU <cengiztoru@gmail.com>
2021-11-10 12:14:23 +0300

    [HMS MAP KIT] Camera functions added

--- app/src/main/java/com/cengiztoru/hmscorekits/ui/map_kit/MapKitActivity.kt
+++ app/src/main/java/com/cengiztoru/hmscorekits/ui/map_kit/MapKitActivity.kt
@@ -2,6 +2,7 @@ package com.cengiztoru.hmscorekits.ui.map_kit
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Point
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -12,11 +13,10 @@ import com.cengiztoru.hmscorekits.utils.Constants
 import com.cengiztoru.hmscorekits.utils.extensions.hideToolBarSetStatusBarTransparent
 import com.cengiztoru.hmscorekits.utils.extensions.isPermissionGranted
 import com.cengiztoru.hmscorekits.utils.extensions.showToast
-import com.huawei.hms.maps.CameraUpdateFactory
-import com.huawei.hms.maps.HuaweiMap
-import com.huawei.hms.maps.MapsInitializer
-import com.huawei.hms.maps.SupportMapFragment
+import com.huawei.hms.maps.*
+import com.huawei.hms.maps.model.CameraPosition
 import com.huawei.hms.maps.model.LatLng
+import com.huawei.hms.maps.model.LatLngBounds
 
 class MapKitActivity : AppCompatActivity() {
 
@@ -58,6 +58,73 @@ class MapKitActivity : AppCompatActivity() {
 
     private fun setMapType(type: Int) {
         mHuaweiMap?.mapType = if (type < 0 || type > 4) HuaweiMap.MAP_TYPE_NORMAL else type
+    }
+
+    private fun cameraFactoryFunctions() {
+        return  //don't call whole function 😊
+        //Increase the camera zoom level by 1 and retain other attribute settings.
+        CameraUpdateFactory.zoomIn()
+
+        // Decrease the camera zoom level by 1 and retain other attribute settings.
+        CameraUpdateFactory.zoomOut()
+
+        // Set the camera zoom level to a specified value and retain other attribute settings.
+        CameraUpdateFactory.zoomTo(8.0f)
+
+        //Increase or decrease the camera zoom level by a specified value.
+        CameraUpdateFactory.zoomBy(2.0f)
+        CameraUpdateFactory.zoomBy(-4.0f)
+
+        // Move the camera to the specified center point and increase or decrease the camera zoom level
+        val point = Point(31, 118)
+        val amount = 2.0f
+        CameraUpdateFactory.zoomBy(amount, point)
+
+        // Set the latitude and longitude of the camera and retain other attribute settings.
+        CameraUpdateFactory.newLatLng(LatLng(31.5, 118.9))
+
+        // Set the visible region and padding.
+        val padding = 100
+        val visibleRegion1 = LatLng(31.5, 118.9)
+        val visibleRegion2 = LatLng(32.5, 119.9)
+        val latLngBounds = LatLngBounds(visibleRegion1, visibleRegion2)
+        CameraUpdateFactory.newLatLngBounds(latLngBounds, padding)
+
+        // Set the center point and zoom level of the camera.
+        val zoom = 3.0f
+        val latLng2 = LatLng(32.5, 119.9)
+        CameraUpdateFactory.newLatLngZoom(latLng2, zoom)
+
+        // Scroll the camera by specified number of pixels.
+        val x = 100.0f
+        val y = 100.0f
+        CameraUpdateFactory.scrollBy(x, y)
+
+        //Specify the camera position.
+        // Set the tilt.
+        val tilt = 2.2f
+        // Set the bearing.
+        val bearing = 31.5f
+        val cameraPosition = CameraPosition(visibleRegion1, zoom, tilt, bearing)
+        CameraUpdateFactory.newCameraPosition(cameraPosition)
+    }
+
+    private fun cameraMovementFunctions(cameraUpdate: CameraUpdate) {
+        val cancelableCallback = object : HuaweiMap.CancelableCallback {
+            override fun onFinish() {}
+
+            override fun onCancel() {}
+        }
+
+        // Move the map camera in animation mode.
+        mHuaweiMap?.animateCamera(cameraUpdate)
+        // Move the map camera in animation mode, and set the API to be called back when the animation stops.
+        mHuaweiMap?.animateCamera(cameraUpdate, cancelableCallback)
+        // Move the map camera in animation mode, and set the animation duration and API to be called back when the animation stops.
+        mHuaweiMap?.animateCamera(cameraUpdate, 250, cancelableCallback)
+
+        // Move the map camera in non-animation mode.
+        mHuaweiMap?.moveCamera(cameraUpdate)
     }
 
 //endregion
